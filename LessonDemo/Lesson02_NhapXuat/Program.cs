276476c967a4ad9fd03a56b8a1a4a96baec9cde5
Chung Trinh <hna.tvchung@gmail.com>
2022-01-18 16:16:57 +0700

    2022-01-18

---
+++ LessonDemo/Lesson02_NhapXuat/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Lesson02_NhapXuat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a, b, c;
+
+            Console.Write("Nhap a=");
+            a =Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap b =");
+            b = int.Parse(Console.ReadLine());
+
+            c = a + b;
+            Console.WriteLine("\n {0} + {1} = {2}", a, b, c);
+            Console.WriteLine("\n {2} ={1} + {0}", a, b, c);
+
+
+            // if
+            if(c%2 == 0)
+            {
+                Console.WriteLine("{0} là số chẵn",c);
+            }
+
+            // if .. else
+            if (c % 2 == 0)
+            {
+                Console.WriteLine("{0} là số chẵn", c);
+            }
+            else
+            {
+                Console.WriteLine("{0} là số le", c);
+            }
+
+            // switch
+
+            // loop: for, do...while, while, foreach
+            Console.WriteLine("day cac so <={0}", a);
+            for (int i = 1; i <= a; i++)
+            {
+                Console.WriteLine("So thu {0}", i);
+            }
+
+            string[] names = { "hung", "dung", "sang", "trong" };
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ LessonDemo/LessonDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LessonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Xin chào, NTUK20CNT");
+
+            // biến, kiểu dữ liệu, hằng số, phép tính,...
+            int a, b, c;
+            int x = 100, y = 200;
+            float p = 123.12f,z;
+            double q = 312.123f;
+            string name = "FIT-NTU-K20";
+
+            // gán giá trị cho biến
+            a = 32;
+            b = 23;
+            c = a + b;
+
+            Console.WriteLine("a=" + a);
+            Console.WriteLine("b=" + b);
+            Console.WriteLine("c=" + c);
+
+            // phép toán số học
+            c = a - b;
+            Console.WriteLine(c);
+            c = a * b;
+            Console.WriteLine(c);
+            z = (float) a / b;
+            Console.WriteLine(z);
+            c = a % b;
+            Console.WriteLine(c);
+            c = a + b;
+            Console.WriteLine(c);
+
+            // phép so sánh
+
+            // phép gán...
+        }
+    }
+}
